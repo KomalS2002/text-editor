cf7e9abce24ffb89e093670754bd3949db13203a
KOmalS2002 <komalsingh10072002@gmail.com>
2022-06-17 18:28:40 +0530

    changed CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 //import logo from './logo.svg';
 //import ReactDOM from 'react-dom/client';
+
 import './App.css';
 import  {useState} from 'react';
 import { Remarkable } from 'remarkable';
+// import { Button } from 'react-bootstrap';
 
 const md= new Remarkable ()
 
@@ -74,50 +76,63 @@ const handleLowerClick = ()=>{
     if(isLowerCase){
       output.style.textTransform = "lowercase"
       setIsLowerCase(false);
+      
     }
     else{
       output.style.textTransform = "none"
       setIsLowerCase(true);
     }
-}
+    }
+    
+  const getText = ()=>{
+    if(document.getSelection){
+      var text= document.getSelection().toString();
+      alert(text);
+    }
+    else{
+      if(document.selection){
+        var text2 = document.selection.creteRange();
+        alert(text2);
+      }
+    }
+  }
 const handleClearClick = ()=>{
   // let output = document.querySelector(".output1")
-  // output.style.textTransform = " "
+  // output.getElementsByClassName("output1").value = " "
+  // document.getElementsByClassName(".output1").value = "";
+  // console.log("good");
  console.log("Clear was clicked"+ text);
   let newText='';
   setText(newText);
+}
+// function color(){document.getElementsByClassName(".output1").style.color = "blue"; }
+const handleColorClick= ()=>{
+  let output=document.querySelector(".output1")
+  output.style.color = "blue";
 }
   return (
     <>
     <div className= 'entity'>
     <h1 className='header'>Text Editor</h1>
-    <div className='complete'>
       <nav className="navbar">
                 <button className="btn1"onClick={handleBoldClick}>B</button>
                 <button className="btn2"onClick={handleItalicClick}>I</button>
                 <button className="btn3"onClick={handleUnderlineClick}>U</button>
-                <button className="btn4"onClick={handleStrikeClick}>Strike</button>
+                <button className="btn4"onClick={handleStrikeClick}>abc</button>
                 <button className="btn5"onClick={handleUpperClick}>AA</button>
                 <button className="btn6"onClick={handleLowerClick}>aa</button>
                 <button className="btn7"onClick={handleClearClick}>Clear</button>
-                {/* <button className="btn8"onClick={handleColorClick}>Color</button> */}
-                <input type="color" className='color'/>
+                <button className='btn8'onClick={getText}>Select</button>
+                <button className="btn9" onClick={handleColorClick}>Color</button>
        </nav>
+    <div className='complete'>
 
-    <div className='container'>
-     
-      
-       
-        {/* <div className='input'>   */}
-       {/* <h3 className='inputText'>Text Input</h3> */}
-       {/* <article > */}
-         {/* <label htmlFor='markdown' className='label'></label> */}
-         <textarea name='textarea' id='markdown' cols="700" rows="10" placeholder='Type some text here' value={text}onChange={(e)=>setText(e.target.value)}
+    
+     <textarea name='textarea' className='container1' cols="700" rows="10" placeholder='Type some text here' value={text}onChange={(e)=>setText(e.target.value)}
          ></textarea>
-         {/* </article> */}
-         {/* </div> */}
-         </div>
-         <div className='container'>        
+         
+        
+         <div className='container2'>        
          <h3 className='output'>Output</h3>
        <div className="output1" dangerouslySetInnerHTML={{__html:md.render(text)}}></div>
        <p className='info'>{text.split(" ").filter((element)=>{return element.length!==0}).length} words {text.length} characters</p>
